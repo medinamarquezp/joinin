fa4c8d7d0d9a2658e1e181294b06f6ed4a758a92
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-05-03 20:33:07 +0200

    fix: error on loading json file

--- client/src/config/dev.config.ts
+++ client/src/config/dev.config.ts
@@ -1,11 +1,19 @@
 import { env } from '$env/dynamic/public';
-import { abi, networks } from '../../../build/contracts/Campaigns.json';
 
-const networkId = '5777' as keyof typeof networks;
+let preloaded = false;
 
 export const config = {
-	networkId,
-	abi,
-	address: networks[networkId].address,
+	networkId: '0',
+	abi: {},
+	address: '0x',
 	storageAPIToken: env.PUBLIC_STORAGE_API_TOKEN
 };
+
+if (process.env.NODE_ENV === 'development' && !preloaded) {
+	const { abi, networks } = await import('../../../build/contracts/Campaigns.json');
+	const networkId = '5777';
+	config.networkId = networkId;
+	config.abi = abi;
+	config.address = networks[networkId].address;
+	preloaded = true;
+}
